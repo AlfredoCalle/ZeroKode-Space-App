8a93e64096afcc7dc9da2c599297bf3200966e71
Alexander Goussas <agoussas@espol.edu.ec>
2021-10-02 23:48:01 -0500

    setup server

--- server/src/main/java/server/App.java
+++ server/src/main/java/server/App.java
@@ -2,13 +2,17 @@ package server;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
+import server.routes.DataController;
 
 public class App extends AbstractVerticle {
     final int PORT = 8080;
 
     @Override public void start() {
         System.out.println("servidor corriendo en el puerto " + PORT);
-        vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(PORT);
+        Router router = DataController.getRouter(vertx);
+        vertx.createHttpServer().requestHandler(router).listen(PORT);
     }
 
     public static void main(String[] args) {
--- server/src/main/java/server/routes/DataController.java
+++ server/src/main/java/server/routes/DataController.java
@@ -1,5 +1,33 @@
 package server.routes;
 
-public class DataController {
+import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
+import static server.model.DataParser.parseData;
+
+public class DataController {
+    private DataController() {}
+
+    public static Router getRouter(Vertx vertx) {
+        Router router = Router.router(vertx);
+        router.get("/api/v1/covid-data").produces("application/json").handler(DataController::sendData);
+        return router;
+    }
+
+    public static void sendData(RoutingContext ctx) {
+        var response = ctx.response();
+        response.putHeader("content-type", "application/json");
+        parseData("/12-31-2020.csv").ifPresentOrElse(data -> {
+            var jsonData = Json.encode(data);
+            response.setStatusCode(200);
+            response.end(jsonData);
+        }, () -> {
+            response.setStatusCode(500);
+            var err = new JsonObject().put("error", "Error reading COVID data");
+            response.end(err.encode());
+        });
+    }
 }
